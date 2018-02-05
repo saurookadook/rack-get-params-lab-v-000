f2dc9a573e34ab3d6dc33f1c1660675b9678f98c
Andy Maskiell <maskiella@gmail.com>
2018-02-05 20:19:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -49,7 +49,6 @@ class Application
   end
 
   # def handle_cart
-      response = []
   #   if @@cart.empty?
   #     "Your cart is empty."
   #   else
@@ -58,7 +57,6 @@ class Application
   #       "#{item}\n"
   #     end
   #   end
-      return 
   # end
 
   # def add_item_to_cart(item_to_add)
